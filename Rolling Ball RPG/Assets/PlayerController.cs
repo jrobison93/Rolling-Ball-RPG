92a16ccf153a1c1a032f949012c55a095b073cd2
llxr458 <llxr@numail.org>
2015-11-13 11:34:59 -0500

    Built Map
    Built the Map

---
+++ Rolling Ball RPG/Assets/PlayerController.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	private float speed;
+	
+	private Rigidbody rb;
+	private int count;
+	
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+        speed = 7;
+		count = 0;
+	}
+
+
+	
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		
+		rb.AddForce (movement * speed);
+	}
+	
+	void OnTriggerEnter(Collider other) 
+	{
+
+		if (other.gameObject.CompareTag ( "Lava"))
+		{
+			transform.position = new Vector3(0, 5, 0);
+			count = count + 1;
+		}
+
+		if (other.gameObject.CompareTag ( "Bounce"))
+		{
+			rb.AddForce(Vector3.up * 800 );
+		}
+
+        if (other.gameObject.CompareTag("Agua"))
+        {
+            rb.drag = 2;
+            Physics.gravity = new Vector3(0, -5, 0);
+        }
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Agua"))
+        {
+            rb.drag = 0;
+            Physics.gravity = new Vector3(0, -10, 0); 
+        }
+
+    }
+
+
+        Vector3 jump = new Vector3(0.0f,400,0.0f);
+	public bool InAir=false;
+
+	void OnCollisionEnter(Collision other){
+		if (other.gameObject.tag == "Untagged"&&InAir==true){
+			InAir = false;
+		}
+	}
+	
+	void OnCollisionStay(Collision other){
+		if (other.gameObject.tag == "Untagged" && InAir == true) {
+			InAir = false;
+		}
+	}
+	
+	void OnCollisionExit(Collision other)
+	{
+		InAir = true;
+	}
+	void Update () {
+		if(Input.GetKeyDown("space")&&InAir==false)
+		{
+			rb.AddForce(jump);
+		}
+	}
+	
+	
+}
---
+++ Rolling Ball RPG/Assets/UpDown.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UpDown : MonoBehaviour {
+
+	public bool Done;
+	private float placex, placey, placez;
+	public float changex, changey, changez;
+
+	void Start()
+	{
+		Done = true;
+		placex = transform.position.x;
+		placey = transform.position.y;
+		placez = transform.position.z;
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		Done = false;
+	}
+
+	void OnCollisionStay()
+	{
+		Done = false;
+		Vector3 up = new Vector3 (placex+changex, placey+changey, placez+changez);
+		transform.position = Vector3.Slerp (transform.position, up, Time.deltaTime);
+	}
+
+	void OnTriggerExit()
+	{
+		StartCoroutine (waiting ());
+	}
+
+	void OnCollisionExit()
+	{
+		StartCoroutine (waiting ());
+	}
+
+
+	void Update () 
+	{		
+		if (Done) 
+		{
+			Vector3 down = new Vector3 (placex, placey, placez);
+			transform.position = Vector3.Slerp (transform.position, down, Time.deltaTime);
+		}
+	}
+
+	IEnumerator waiting()
+	{
+		yield return new WaitForSeconds(0.5f);
+		Done = true;
+	}
+}
